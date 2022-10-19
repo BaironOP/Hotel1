32a15b0f0526ea74888452c3c03e9b23a1d35aa4
BaironOP <113804675+BaironOP@users.noreply.github.com>
2022-10-18 20:02:45 -0500

    Nuevo proyecto con libreria

---
+++ src/control/FormularioControler.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package control;
+
+import java.util.ArrayList;
+import model.*;
+import view.Formulario;
+
+/**
+ *
+ * @author Usuario
+ */
+public class FormularioControler {
+    
+    private ArrayList<Habitacion> habitaciones = new ArrayList<>();
+    private ArrayList<Usuario> usuarios = new ArrayList<>();
+    private Formulario vista = new Formulario();
+
+    public void ingresarUsuario(Usuario n){
+        usuarios.add(n);
+    }
+}
